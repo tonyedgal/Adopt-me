847175e154e23d6a6c6fcba8dc825e1c82c74ed8
Tony Edgal <78075405+tonyedgal@users.noreply.github.com>
2021-11-07 22:06:22 +0100

    updated jsx in both child and parent component

--- src/App.js
+++ src/App.js
@@ -3,23 +3,13 @@ import ReactDOM from "react-dom";
 import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Spiky",
-      animal: "Dog",
-      breed: "ihasa Apso",
-    }),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      name: "Baby",
-      animal: "Dog",
-      breed: "German Shepherd",
-    }),
-  ]);
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Spiky" animal="Dog" breed="Ihasa Apso" />
+      <Pet name="Luna" animal="Dog" breed="Havanese" />
+      <Pet name="Baby" animal="Dog" breed="German Shepherd" />
+    </div>
+  );
 };
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
