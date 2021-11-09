f78db14a917946178a28b9ea745e6f06cd48d287
Tony Edgal <78075405+tonyedgal@users.noreply.github.com>
2021-11-09 04:00:15 +0100

    feat: installed eslint react plugin, jsx a11y and react hooks dependency and implemented hooks with useState

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Pet from "./Pet";
+import { render } from "react-dom";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Spiky" animal="Dog" breed="Ihasa Apso" />
-      <Pet name="Luna" animal="Dog" breed="Havanese" />
-      <Pet name="Baby" animal="Dog" breed="German Shepherd" />
+      <SearchParams />
     </div>
   );
 };
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
---
+++ src/SearchParams.js
@@ -0,0 +1,67 @@
+import { useState } from "react";
+
+const ANIMALS = ["dog", "cat", "bird", "goat", "ram", "fish"];
+
+const SearchParams = () => {
+  const [location, setLocation] = useState("Seattle, WA");
+  // Use hooks without destructuring:
+  // const locationTuple = useState ("Seatle, WA")
+  // const location = locationTuple[0];
+  // const setLocation = locationTuple[1];
+  const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
+  const breeds = [];
+
+  return (
+    <div className="search-params">
+      <form>
+        <label htmlFor="location">
+          Location
+          <input
+            id="location"
+            onChange={(e) => setLocation(e.target.value)}
+            value={location}
+            placeholder="Location"
+          />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => setAnimal(e.target.value)}
+            onBlur={(e) => setAnimal(e.target.value)}
+          >
+            <option />{" "}
+            {/* represents empty option in html <option value=""></option>*/}
+            {ANIMALS.map((animal) => (
+              <option value={animal} key={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select
+            id="breed"
+            value={breed}
+            onChange={(e) => setBreed(e.target.value)}
+            onBlur={(e) => setBreed(e.target.value)}
+          >
+            <option />{" "}
+            {/* represents empty option in html <option value=""></option>*/}
+            {breeds.map((breed) => (
+              <option value={breed} key={breed}>
+                {breed}
+              </option>
+            ))}
+          </select>
+        </label>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchParams;
