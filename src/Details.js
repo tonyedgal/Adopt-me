958661b5ad28c5c51feb266fd4866396a08a0b13
Tony Edgal <78075405+tonyedgal@users.noreply.github.com>
2021-11-21 10:37:49 +0100

    feat: managed state in stateful class component Carousel.js and set defaultProps

--- src/Details.js
+++ src/Details.js
@@ -1,5 +1,6 @@
 import { Component } from "react";
 import { withRouter } from "react-router";
+import Carousel from "./Carousel";
 
 class Details extends Component {
   state = { loading: true };
@@ -23,9 +24,11 @@ class Details extends Component {
       return <h2>Loading...</h2>;
     }
 
-    const { animal, breed, city, state, description, name } = this.state;
+    const { animal, breed, city, state, description, name, images } =
+      this.state;
     return (
       <div className="details">
+        <Carousel images={images} />
         <div>
           <h1>{name}</h1>
           <h2>{`${animal} - ${breed} - ${city}, ${state}`}</h2>
