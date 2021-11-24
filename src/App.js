277ebeecb90817df3227dc3a4c6acfcb65190a6c
Tony Edgal <78075405+tonyedgal@users.noreply.github.com>
2021-11-24 23:12:54 +0100

    update: playing around with context on different components

--- src/App.js
+++ src/App.js
@@ -21,7 +21,9 @@ const App = () => {
               <Details />
             </Route>
             <Route path="/">
-              <SearchParams />
+              <ThemeContext.Provider value={["gray"]}>
+                <SearchParams />
+              </ThemeContext.Provider>
             </Route>
           </Switch>
         </Router>
